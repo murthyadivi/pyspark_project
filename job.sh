14008c3942d68dfde3c51c2f2ae00fe2d3218cd5
murthyadivi <murthy.adivirk@gmail.com>
2020-01-26 16:26:31 +0530

    Add files via upload

--- job.sh
+++ job.sh
@@ -1,11 +1,18 @@
-sqoop import --connect jdbc:mysql://cxln2.c.thelab-240901.internal/sqoopex --username sqoopuser --password-file /user/reallegendscorp9155/sqoop.password  --split-by data_value --columns series_reference,period,data_value,status,units,magnitude,subject,groups,series_title_1,series_title_2,series_title_3,series_title_4,series_title_5 --table local_finance --delete-target-dir --target-dir /user/reallegendscorp9155/murthy/project/local_data --fields-terminated-by "," --hive-import --hive-overwrite --hive-table murthy_project.local_finance
+sqoop import --bindir /usr/lib/sqoop --connect jdbc:mysql://localhost:3306/sqoopex --username root --password-file /root/etl-offload/sqoop.password  --split-by data_value --columns series_reference,period,data_value,status,units,magnitude,subject,groups,series_title_1,series_title_2,series_title_3,series_title_4,series_title_5 --table local_finance --delete-target-dir --target-dir /root/sqoopex_import/local_data --fields-terminated-by "," --hive-import --hive-overwrite --hive-table etloffload.local_finance
 
 
 
-sqoop import --connect jdbc:mysql://cxln2.c.thelab-240901.internal/sqoopex --username sqoopuser --password-file /user/reallegendscorp9155/sqoop.password  --split-by data_value --columns series_reference,period,data_value,status,units,magnitude,subject,groups,series_title_1,series_title_2,series_title_3,series_title_4,series_title_5 --table central_finance --delete-target-dir --target-dir /user/reallegendscorp9155/murthy/project/central_data --fields-terminated-by "," --hive-import --hive-overwrite --hive-table murthy_project.central_finance
 
 
-sqoop import --connect jdbc:mysql://cxln2.c.thelab-240901.internal/sqoopex --username sqoopuser --password-file /user/reallegendscorp9155/sqoop.password  --split-by data_value --columns series_reference,period,data_value,status,units,magnitude,subject,groups,series_title_1,series_title_2,series_title_3,series_title_4,series_title_5 --table general_finance --delete-target-dir --target-dir /user/reallegendscorp9155/murthy/project/general_data --fields-terminated-by "," --hive-import --hive-overwrite --hive-table murthy_project.general_finance
+sqoop import --bindir /usr/lib/sqoop --connect jdbc:mysql://localhost:3306/sqoopex --username root --password-file /root/etl-offload/sqoop.password  --split-by data_value --columns series_reference,period,data_value,status,units,magnitude,subject,groups,series_title_1,series_title_2,series_title_3,series_title_4,series_title_5 --table central_finance --delete-target-dir --target-dir /root/sqoopex_import/central_data --fields-terminated-by "," --hive-import --hive-overwrite --hive-table etloffload.central_finance
 
 
-spark-submit /home/reallegendscorp9155/murthy_project/py_spark.py
+
+
+sqoop import --bindir /usr/lib/sqoop --connect jdbc:mysql://localhost:3306/sqoopex --username root --password-file /root-offload/sqoop.password  --split-by data_value --columns series_reference,period,data_value,status,units,magnitude,subject,groups,series_title_1,series_title_2,series_title_3,series_title_4,series_title_5 --table general_finance --delete-target-dir --target-dir /root.sqoopex_import/general_data --fields-terminated-by "," --hive-import --hive-overwrite --hive-table etloffload.general_finance
+
+
+spark-submit 
+
+/root/etl-offload/py_spark.py
+
